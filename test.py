7d57a95335b4407d4768d7f04f1f8de6be040374
imds0 <ivandrelichman@gmail.com>
2018-05-07 22:43:48 -0400

    Prepating the template for Regex

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ for item in text:
 text = ['Satosh Nakamoto','Alice Nakamoto','RoboCop Nakamoto','satoshi Nakamoto','Mr. Nakamoto','Nakamoto', 'Satoshi nakamoto']
 for item in text:
     numsRegex = re.compile(r'''(
-        (^\d{1,3})      # Expresion n
-        (,\d{3})*$      # Expresion n
+        ()      # Expresion n
+        ()     # Expresion n
         )''', re.VERBOSE)
     mo21 = numsRegex.findall(item)
     print(mo21)
@@ -24,8 +24,8 @@ for item in text:
 text = ['Alice eats apples.','Bob pets cats.','Carol throws baseballs.','Alice throws Apples.','BOB EATS CATS.','RoboCop eats apples.', 'ALICE THROWS FOOTBALLS.', 'Carol eats 7 cats.']
 for item in text:
     numsRegex = re.compile(r'''(
-        (^\d{1,3})      # Expresion n
-        (,\d{3})*$      # Expresion n
+        ()      # Expresion n
+        ()      # Expresion n
         )''', re.VERBOSE)
     mo22 = numsRegex.findall(item)
     print(mo22)

6adeae8158642b7b69d51a305d3527ce8bb0e55c
imds0 <ivandrelichman@gmail.com>
2018-05-10 20:02:10 -0400

    Seting up the notebook

---
+++ Chapter_8/randomQuizGenerator.py
@@ -0,0 +1,7 @@
+#! python3
+# randomQuizGenerator.py - Creates quizzes with questions and aswers in
+# random order, along with the answer key.
+
+import random
+
+# The quiz data. Keys are states and values are their capitals.

fa4f5fc3874385d625b2c5abda77b4ef7cd81b5f
imds0 <ivandrelichman@gmail.com>
2018-05-12 17:27:51 -0400

    Day 13, randomQuizGenerator TODO: Comment and modify the code to make it about anytopic

--- test.py
+++ test.py
@@ -9,8 +9,52 @@ import os
 # helloFile.close()
 # sonnetFile = open((r'D:\Drive\Code\ATBSWP\Chapter_8\sonnet29.txt'),'a')
 # print(sonnetFile.readlines())
-os.chdir(r'D:\Drive\Code\ATBSWP\Chapter_8')
-baconFile = open('bacon.txt','w')
-baconFile.write('Hello world!\n')
-baconFile.close()
+
+# os.chdir(r'D:\Drive\Code\ATBSWP\Chapter_8')
+# baconFile = open('bacon.txt','w')
+# baconFile.write('Hello world!\n')
+# baconFile.close()
+
+# def is1to9(item):
+#     while True:
+#         print('Please enter a number  between 1 and 9 for ' + item + '.' )
+#         number = input()
+#         if (number.isdigit()) and (1 <= int(number) <= 9) and (int(number) == float(number)):
+#             break
+#     return int(number)
+
+# cube = [
+#     {'L':'TL','M':'TM','R':'TR',},
+#     {'L':'ML','M':'MM','R':'MR',},
+#     {'L':'LL','M':'LM','R':'LR',},
+#     ]
+# listofValues = []
+# for row in range(len(cube)):
+#     listofValues += list(cube[row].values())
+# print(listsss)
+
+import random
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+    'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+    'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+    'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+    'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+    'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+    'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+    'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+    'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+    'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
+    'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+states = list(capitals.keys())
+print(states)
+random.shuffle(states)
+print(states)
+random.shuffle(states)
+print(states)
 input()

8d57c769063881eaa957772c20d146f2f97ab9bd
imds0 <ivandrelichman@gmail.com>
2018-05-17 21:28:34 -0400

    Day 17, renameDates2 working, it allows to input a directory to search (only print not shutil.move)

--- Chapter_9/renameDates2.py
+++ Chapter_9/renameDates2.py
@@ -1,27 +1,45 @@
 #! python3
 # renameDates2.py  - First try of renameDates without looking the code first
-import re, os, shutil
-path = ''
-# TODO: Search filenames in all subfoldes from one folder
-    # TODO: Regex to find dates FROM MM-DD-YYYY to DD-MM-YYYY
-text = 'YO10-05-2018.docs, 11-30-2018'
-datesRegex = re.compile(r'''(
-        (.*?)           # Everything before Group 2    #Difference between (.*)?  Optional - (.*?)      Nongreedy
-        ([0|1][0|1|2])  # Months Group 3
-        -
-        ([0|1|2|3][0-9])    # Days Group 4 
-        -
-        ([0|1|2][0-9]{3})   # Years Group 5
-        (.*?)               # Everything afterGroup 6
-        )''', re.VERBOSE)
-mo = datesRegex.findall(text)
-print(mo)
-    # TODO: List of the filenames 
-for folderName, subFolders, filenames in os.walk(path):
-    for filename in filenames:
-        if filename in datesRegex(text):
-            newName = ''
-            shutil.move(filename,'.\\%s\\%s' % (folderName,newName))
-            print(filename,'.\\%s\\%s' % (folderName))
 
-# TODO: When a file is found it is renamed with the European-style
+import re, os, shutil
+
+dateRegex = re.compile(r'''
+        ^(.*?)              # Everything before    #Difference between (.*)?  Optional - (.*?)      Nongreedy)
+        ([0|1][0-9])-       # Months Group 2
+        ([0|1|2|3][0-9])-   # Days Group 3 
+        ([0|1|2][0-9]{3})   # Years Group 4
+        (.*?)$              # TODO: No space
+        ''', re.VERBOSE)
+
+
+print('Enter the folder to check')
+folderToCheck = (r'%s' %(input()))
+# folderToCheck = r"D:\Drive\Code\ATBSWP\Chapter_9"
+# os.chdir(os.path.abspath(folderToCheck))
+# os.chdir(r'\..')
+# print(os.getcwd())
+
+for folderName, subfoldersName, files in os.walk(folderToCheck):    # CWD does not include subfolders but renames folders not only files
+    # print(subfoldersName)
+    for subfolder in subfoldersName:
+        mo3 = dateRegex.search(subfolder)
+        if mo3 != None:
+                newName = mo3.group(1)+mo3.group(3)+'-'+mo3.group(2)+'-'+mo3.group(4)+mo3.group(5)
+                fromFile = os.path.join(folderName,mo3.group(0)) 
+                toFile = os.path.join(folderName,newName)
+                print('----- Renaming folder "%s" to "%s"\\... -----' %(fromFile,toFile))
+                # shutil.move(fromFile,toFile)
+
+    # """
+    for file in files:
+        # print(file)
+        mo3 = dateRegex.search(file)
+        if mo3 != None:
+            newName = mo3.group(1)+mo3.group(3)+'-'+mo3.group(2)+'-'+mo3.group(4)+mo3.group(5)
+            fromFile = os.path.join(folderName,mo3.group(0))    # mo3.group(0) = file = oldFileName
+            toFile = os.path.join(folderName,newName)
+            print('----- Renaming file "%s" to "%s"\\... -----' %(fromFile,toFile))
+            # shutil.move(fromFile,toFile)
+
+    # """
+input()
--- test.py
+++ test.py
@@ -1,39 +1,42 @@
-import zipfile, os, re
-text = 'YO10-05-2018.docs, 91-30-2018'
-datesRegex = re.compile(r'''(
-        ^(.*?)           # Everything before    #Difference between (.*)?  Optional - (.*?)      Nongreedy
-        ([0|1][0|1|2])  # Months Group 2
-        -
-        ([0|1|2|3][0-9])    # Days Group 3 
-        -
+import re, os, shutil
+
+dateRegex = re.compile(r'''
+        ^(.*?)              # Everything before    #Difference between (.*)?  Optional - (.*?)      Nongreedy)
+        ([0|1][0-9])-       # Months Group 2
+        ([0|1|2|3][0-9])-   # Days Group 3 
         ([0|1|2][0-9]{3})   # Years Group 4
-        (.*?) #no space
-        )''', re.VERBOSE)
-mo = datesRegex.search(text)
-print(mo.group())
-newName = str(mo.group(2))+str(mo.group(4))+'-'+str(mo.group(3))+'-'+str(mo.group(5))+str(mo.group(6))
-print(newName)
-
-datePattern = re.compile(r'''^(.*?) # all thext before the date
-    ((0|1)?\d)-                     # one or two digits for the month
-    ((0|1|2|3)?\d)-                 # one or two digits for the day
-    ((19|20)\d\d)                   # four digits for the year
-    (.*?)$                          # all text after the date
-    ''', re.VERBOSE)
-
-mo2 = datePattern.search(text)
-print('Here comes MO2')
-print(mo2.group)    
-print('That was dissapointing')
+        (.*?)$              # TODO: No space
+        ''', re.VERBOSE)
 
 
-for folderName, subfoldersName, files in os.walk('.\Chapter_9'):    # CWD does not include subfolders but renames folders not only files
-    for file in files:
-        print(folderName +'\\'+ str(file))      # TODO: Function join
-        mo3 = datePattern.search(file)
+print('Enter the folder to check')
+folderToCheck = (r'%s' %(input()))
+# folderToCheck = r"D:\Drive\Code\ATBSWP\Chapter_9"
+# os.chdir(os.path.abspath(folderToCheck))
+# os.chdir(r'\..')
+# print(os.getcwd())
+
+for folderName, subfoldersName, files in os.walk(folderToCheck):    # CWD does not include subfolders but renames folders not only files
+    # print(subfoldersName)
+    for subfolder in subfoldersName:
+        mo3 = dateRegex.search(subfolder)
         if mo3 != None:
-                print('---Do---')
-                print(mo3)
+                newName = mo3.group(1)+mo3.group(3)+'-'+mo3.group(2)+'-'+mo3.group(4)+mo3.group(5)
+                fromFile = os.path.join(folderName,mo3.group(0)) 
+                toFile = os.path.join(folderName,newName)
+                print('----- Renaming folder "%s" to "%s"\\... -----' %(fromFile,toFile))
+                # shutil.move(fromFile,toFile)
 
+    # """
+    for file in files:
+        # print(file)
+        mo3 = dateRegex.search(file)
+        if mo3 != None:
+            newName = mo3.group(1)+mo3.group(3)+'-'+mo3.group(2)+'-'+mo3.group(4)+mo3.group(5)
+            fromFile = os.path.join(folderName,mo3.group(0))    # mo3.group(0) = file = oldFileName
+            toFile = os.path.join(folderName,newName)
+            print('----- Renaming file "%s" to "%s"\\... -----' %(fromFile,toFile))
+            # shutil.move(fromFile,toFile)
 
+    # """
 input()

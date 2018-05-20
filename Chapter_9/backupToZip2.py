05736dc5e5cece139e06d3c78046492515384ff3
imds0 <ivandrelichman@gmail.com>
2018-05-19 22:00:14 -0400

    Day  19, finishing chapter 9 and working on backupToZip2

--- Chapter_9/backupToZip2.py
+++ Chapter_9/backupToZip2.py
@@ -1,14 +1,13 @@
 #! python3
 # backuptoZip2.py - Personal version of backupToZip.py
-# TODO: Comment the code
-# TODO: Use shelve module to save last index
-# TODO: Somethin first time running -> no test.zip
+
+
 import os,zipfile,re
 
-# toBackup = r'%s' % input()
-toBackup = r'D:\Drive\Code\ATBSWP\Chapter_9\test'
+# toBackup = r'%s' % input()    # Allows the user to input the directory to backup
+toBackup = r'D:\Drive\Code\ATBSWP\Chapter_9\test'   # Test directory
 os.chdir(toBackup)
-foldertoBackup = os.path.split(toBackup)[1] # folder to backup 'folder_name'
+foldertoBackup = os.path.split(toBackup)[1] # folderToBackup = 'folder_name', sabe as os.path.basename(toBackup)
 
 # Regex to look the version
 VersionRegex = re.compile(r'''
@@ -18,28 +17,35 @@ VersionRegex = re.compile(r'''
     .zip
     '''%foldertoBackup,re.VERBOSE)
 
-# Loop in the folder
+# Loops in the folder
+dirTolook = os.listdir(toBackup) # Lists the content of the folder to iterate everyone 
 
-dirTolook = os.listdir(toBackup)
+# Search for the last version
 allVer = []
 for files in dirTolook:
     mo = VersionRegex.search(files)
     if mo != None:
-        allVer = allVer + [mo.group()]
+        allVer = allVer + [mo.group()]  # allVer is a list of versions
         # print(mo.group(2))
-if VersionRegex.search(allVer[-1]).group(2) == '':
+
+if allVer == []:    # first time running
     lastVersion = 0
-else:
-    lastVersion = VersionRegex.search(allVer[-1]).group(2)
+else:               
+    lastVersion = VersionRegex.search(allVer[-1]).group(2)  # Last version is the last of the list, pass the Regex group(2) to find the version number
 
-
+# Create the new zip
 zipName = foldertoBackup+'_'+str(int(lastVersion)+1)+'.zip'
 newZip = zipfile.ZipFile('%s' %zipName,'w')
 
-for files in dirTolook:
-    newZip.write(files,compress_type=zipfile.ZIP_DEFLATED)
 
+for files in dirTolook: # Adds the files to the newZip
+    if (files+'_').startswith(foldertoBackup) and files.endswith('.zip'): # skips the previous backups
+        continue
+    else:
+        print('Adding %s ' % files)
+    newZip.write(files,compress_type=zipfile.ZIP_DEFLATED)
 newZip.close()
+
 
 print('Done')
 input()
--- littleTest.py
+++ littleTest.py
@@ -1,23 +1,8 @@
-import re
-dateRegex = re.compile(r'''
-        ^(.*?)              # Everything before    #Difference between (.*)?  Optional - (.*?)      Nongreedy)
-        ([0|1][0-9])-    # Months Group 2
-        ([0|1|2|3][0-9])-  # Days Group 3 
-        ([0|1|2][0-9]{3})   # Years Group 4
-        (.*?)$              # TODO: No space
-        ''', re.VERBOSE)
-
-
-datePattern = re.compile(r'''
-    ^(.*?) # all thext before the date
-    ((0|1)?\d)-                     # one or two digits for the month
-    ((0|1|2|3)?\d)-                 # one or two digits for the day
-    ((19|20)\d\d)                   # four digits for the year
-    (.*?)$                          # all text after the date
-    ''', re.VERBOSE)
-
-test = 'A13-21-2018Spam Test - Copy.txt'
-test2 = '04-24-2018'
-mo1 = dateRegex.search(test)
-print(mo1.group(4))
+thelist = ['no', 'test1', 'test2']
+i=3
+if 'test%s' % (str(i)) in thelist:
+    thelist.remove('test%s' % (str(i)))
+test = 'yonose'
+print(test[:100])
+print(thelist)
 input()

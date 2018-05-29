85ea54f957bc2f4ed1223d5a3938428d83550af2
imds0 <ivandrelichman@gmail.com>
2018-05-29 10:02:36 -0400

    Day 27, trying things with bs4

--- Chapter_11/lucky.py
+++ Chapter_11/lucky.py
@@ -4,10 +4,17 @@
 import requests, sys, webbrowser, bs4
 
 print('Googling...') # display text while downloading the google page
-res = requests.get('http://google.com/search?q=' + ''.join(sys.argv[1:]))
+search = ''.join(sys.argv[1:])
+search = 'beautiful soup'
+res = requests.get('http://google.com/search?q=' + search)
 res.raise_for_status()
 
-#TODO: Retrieve top sear result links.
+# Retrieve top sear result links.
+soup = bs4.BeautifulSoup(res.text,'html.parser')
+#Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = min(5,len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
 
-#TODO: Open a browser tab for each result.
-
+print('Done')
--- Chapter_11/lucky2.py
+++ Chapter_11/lucky2.py
@@ -3,8 +3,14 @@ import sys, bs4, webbrowser, requests
 
 if len(sys.argv) > 1:
     search = ' '.join(sys.argv[1:])
+    search = 'beautiful soup'
     googleLink = 'https://www.google.com/search?q=' + search
     results = requests.get(googleLink)
     results.raise_for_status()
     results = bs4.BeautifulSoup(results.text,"html.parser")
-    print(type(results))
+    link = results.select('.r a')
+    for i in range(3):
+        url = 'https://www.google.com' + link[i].get('href')
+        print('opening %s...' % url)
+        # webbrowser.open(url)
+    print('Done')

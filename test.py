31d93b60fb8a0fa76611d73bc9170b0f68d9047a
unknown <ivandrelichman@gmail.com>
2018-06-05 16:08:10 -0400

    Day 33, still working on personal project, can't make selenium wait

--- test.py
+++ test.py
@@ -33,3 +33,7 @@ import traceback
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+
+browser = webdriver.Firefox()
+browser.get('http//google.com')
+browser.implicitly_wait(150)

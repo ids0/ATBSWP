0b29bbcbf2aa5551f4cb6192807365f74c5dfbb5
unknown <ivandrelichman@gmail.com>
2018-06-17 18:47:38 -0400

    Day 43, combinePdfs.py

--- test.py
+++ test.py
@@ -1,38 +1,16 @@
-import os, openpyxl
-from openpyxl.styles import Font
-os.chdir('D:/Drive/Code/ATBSWP/Chapter_12')
-wb = openpyxl.Workbook()
-sheet = wb['Sheet']
-italic24Font = Font(size=24, italic=True)
-
-fontObj1 = Font(name='Times New Roman',bold=True)
-sheet['A1'].font = fontObj1
-sheet['A1'] = 'Bold Times New Roman'
-
-fontObj2 = Font(size=24, italic=True)
-sheet['B3'].font = fontObj2
-sheet['B3'] = '24 pt Italic'
-
-sheet['A1'] = 'Tall  row'
-sheet['A2'] = 300
-sheet['A3'] = '=SUM(A2:A2)'
-sheet['B2'] = 'Wide Column'
-
-# sheet.row_dimensions[1].height = 70
-# sheet.column_dimensions['B'].width =50
-
-sheet.merge_cells('A1:D3')
-sheet['A1'] = 'Twelve cells merge together'
-sheet['D3'] = 'Twelve cells merge together?!!!'
-sheet.merge_cells('C5:D5')
-sheet['C5'] = 'Two merged cells.'
-sheet['D5'] = 'Hidden cell?'
-sheet.unmerge_cells('C5:D5')
+import os, PyPDF2
+os.chdir('D:/Drive/Code/ATBSWP/Chapter_13')
+minutesFile = open('meetingminutes.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(minutesFile)
+pdfWriter = PyPDF2.PdfFileWriter()
+for pageNum in range(pdfReader.getNumPages()):
+    pageObj = pdfReader.getPage(pageNum).rotateClockwise(90*(pageNum+1))
+    pdfWriter.addPage(pageObj)
 
 
-wb.save('styled.xlsx')
+resultPdfFile = open('rotatedPage.pdf', 'wb')
+pdfWriter.write(resultPdfFile)
+resultPdfFile.close()
+minutesFile.close()
 
-wb = openpyxl.load_workbook('produceSales.xlsx')
-sheet = wb.active
-sheet.freeze_panes = 'A2'
-wb.save('freezeExample.xlsx')
+input()
